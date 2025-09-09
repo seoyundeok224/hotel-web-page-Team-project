23979d5c0c7304660258517910d9c5b6ee82a21d
sjh(ys) <sjh6226@naver.com>
2025-09-09 10:53:15 +0900

    스프링 새프로젝트

---
+++ devHotel-spring/src/main/java/kr/or/ysedu/devHotel_spring/DevHotelSpringApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ysedu.devHotel_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHotelSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevHotelSpringApplication.class, args);
+	}
+
+}
---
+++ devHotel-spring/src/test/java/kr/or/ysedu/devHotel_spring/DevHotelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.ysedu.devHotel_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHotelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
