134fabcb58cae1723e7236d03b00ab384c163a8e
sjh(ys) <sjh6226@naver.com>
2025-09-15 10:08:49 +0900

    change-gradle

---
+++ hotel/src/main/java/kr/or/hotelpms/hotel/HotelApplication.java
@@ -0,0 +1,13 @@
+package kr.or.hotelpms.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelApplication.class, args);
+	}
+
+}
